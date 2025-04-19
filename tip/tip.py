56d1e85dd9f5946c97c6d8cbe1334451752f0d8e
markam303 <marcin93kaminski@gmail.com>
2025-04-19 14:20:53 +0000

    Apr 19, 2025, 4:20 PM

---
+++ tip/tip.py
@@ -0,0 +1,19 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    dollars_stripped_to_numbers = d.strip("$")
+    return float(dollars_stripped_to_numbers)
+
+
+def percent_to_float(p):
+    percents_stripped_to_numbers = p.strip("%")
+    return float(percents_stripped_to_numbers)
+
+    
+
+main()
