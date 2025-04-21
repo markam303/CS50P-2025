4bdf4e62147cbc9860e96b7625e7c1e6029a6796
markam303 <marcin93kaminski@gmail.com>
2025-04-21 19:12:22 +0000

    Apr 21, 2025, 9:12 PM

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,5 +3,8 @@
 filename = input("File name: ").lower().strip()
 
 match filename:
-    case filename.endswith(".gif"):
+    case filename.endswith("gif"):
         print("image/gif")
+    case filename.endswith("jpg"):
+        print("image/jpg")
+        
