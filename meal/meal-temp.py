404f61bed0afe45f0a4ba16539f967a0b324e96d
markam303 <marcin93kaminski@gmail.com>
2025-04-21 21:08:09 +0000

    Apr 21, 2025, 11:08 PM

---
+++ meal/meal-temp.py
@@ -0,0 +1,26 @@
+### Input time in 24h time ###
+
+def main():
+    time = input("What time is it? ").strip()
+    
+    time = convert(time)
+    
+    if time >= 7.00 and time <= 8.00:
+        print("breakfast time")
+    elif time >= 12.00 and time <= 13.00:
+        print("lunch time")
+    elif time >= 18.00 and time <= 19.00:
+        print("dinner time")
+    else:
+        return 0
+    
+def convert(time):
+    hour, min = time.split(":")
+    hour = float(hour)
+    min = float(min)
+    min = min / 60
+    return float(hour + min)
+    
+
+if __name__ == "__main__":
+    main()
--- meal/meal.py
+++ meal/meal.py
@@ -1,18 +1,36 @@
-### Input time in 24h time ###
+### Input time in 24h time and 12h time ###
 
 def main():
     time = input("What time is it? ").strip()
     
-    time = convert(time)
+    try:
+        time, am_pm = time.split(" ")
+        
+        time = convert(time)
+        
+        if am_pm == "a.m." or am_pm == "A.M.":
+            if time >= 7.00 and time <= 8.00:
+                print("breakfast time")
+            elif time >= 12.00:
+                print("lunch time")
+            else:
+                return 0
+        elif am_pm == "p.m." or am_pm == "P.M.":
+            if time >= 6.00 and time <= 7.00:
+                print("dinner time")
+    except ValueError:
+        time = convert(time)
     
-    if time >= 7.00 and time <= 8.00:
-        print("breakfast time")
-    elif time >= 12.00 and time <= 13.00:
-        print("lunch time")
-    elif time >= 18.00 and time <= 19.00:
-        print("dinner time")
-    else:
-        return 0
+        if time >= 7.00 and time <= 8.00:
+            print("breakfast time")
+        elif time >= 12.00 and time <= 13.00:
+            print("lunch time")
+        elif time >= 18.00 and time <= 19.00:
+            print("dinner time")
+        else:
+            return 0
+        
+
     
 def convert(time):
     hour, min = time.split(":")
