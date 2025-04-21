2211f5474e6d3fd08e0094a0bb8ab742d791d55e
markam303 <marcin93kaminski@gmail.com>
2025-04-21 20:08:56 +0000

    Apr 21, 2025, 10:08 PM

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,17 @@
 ### Math interpreter: input formua as X Y Z ###
 
-expression = input("Expression: ").split()
+expression = input("Expression: ").strip()
+
+x, y, z = expression.split(" ")
+x = float(x)
+z = float(z)
+match y:
+    case "+":
+        print(round(x + z, 1))    
+    case "-":
+        print(round(x - z, 1)) 
+    case "*":
+        print(round(x * z, 1))   
+    case "/":
+        print(round(x / z, 1))     
+        
