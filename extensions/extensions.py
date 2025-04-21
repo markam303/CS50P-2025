0164ed32d89a516a9548d0e414ffde93a63af4da
markam303 <marcin93kaminski@gmail.com>
2025-04-21 19:22:29 +0000

    Apr 21, 2025, 9:22 PM

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,4 +6,15 @@ if filename.endswith(".gif"):
     print("image/gif")
 elif filename.endswith(".jpg"):
     print("image/jpg")
-        
+elif filename.endswith(".jpeg"):
+    print("image/jpeg")
+elif filename.endswith(".png"):
+    print("image/png")
+elif filename.endswith(".pdf"):
+    print("application/pdf")
+elif filename.endswith(".txt"):
+    print("text/plain")
+elif filename.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
