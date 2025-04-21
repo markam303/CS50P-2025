b191e0ef896628feb9a2d0c09e9b2482fd22b84e
markam303 <marcin93kaminski@gmail.com>
2025-04-21 20:59:49 +0000

    Apr 21, 2025, 10:59 PM

--- meal/meal12h.py
+++ meal/meal12h.py
@@ -10,12 +10,14 @@ def main():
     if am_pm == "a.m" or am_pm == "A.M.":
         if time >= 7.00 and time <= 8.00:
             print("breakfast time")
-        elif time >= 12.00 and time <= 13.00:
+        elif time >= 12.00:
             print("lunch time")
-        elif time >= 18.00 and time <= 19.00:
-            print("dinner time")
         else:
-            pass
+            return 0
+    elif am_pm == "p.m." or am_pm == "P.M.":
+        if time >= 6.00 and time <= 7:00
+        print("dinner time")
+
     
 def convert(time):
     hour, min = time.split(":")
