5c808d55041bdb0856ce8386af234bbde6d4fa08
markam303 <marcin93kaminski@gmail.com>
2025-04-21 20:50:46 +0000

    Apr 21, 2025, 10:50 PM

---
+++ meal/meal12h.py
@@ -0,0 +1,28 @@
+### Input time in 12h time ###
+
+def main():
+    time = input("What time is it? ").strip()
+    
+    time, am_pm = time.split(" ")
+    
+    time = convert(time)
+    
+    if time >= 7.00 and time <= 8.00:
+        print("breakfast time")
+    elif time >= 12.00 and time <= 13.00:
+        print("lunch time")
+    elif time >= 18.00 and time <= 19.00:
+        print("dinner time")
+    else:
+        pass
+    
+def convert(time):
+    hour, min = time.split(":")
+    hour = float(hour)
+    min = float(min)
+    min = min / 60
+    return float(hour + min)
+    
+
+if __name__ == "__main__":
+    main()
