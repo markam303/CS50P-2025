894188801dacc14fd7a0f6aae063aee2f5d82d44
markam303 <marcin93kaminski@gmail.com>
2025-04-21 20:10:29 +0000

    Apr 21, 2025, 10:10 PM

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,8 +3,8 @@
 expression = input("Expression: ").strip()
 
 x, y, z = expression.split(" ")
-x = float(x)
-z = float(z)
+x = int(x)
+z = int(z)
 match y:
     case "+":
         print(round(x + z, 1))    
