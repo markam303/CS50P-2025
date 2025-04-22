fc4521ef5c47c4636a4c62ab82c5810d52ca0a6f
markam303 <marcin93kaminski@gmail.com>
2025-04-22 15:57:27 +0000

    Apr 22, 2025, 5:57 PM

---
+++ coke/coke.py
@@ -0,0 +1,11 @@
+### Coke machine that checks for amount due from the user. Cost of 1 cola is 50 cents ###
+
+def main():
+    cola = 50
+    coins = [25, 10, 5]
+    print(amount_due)
+
+
+
+
+main()
