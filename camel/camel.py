d68c4610dbd883289a0e18f11ad4eb59bcf97c0d
markam303 <marcin93kaminski@gmail.com>
2025-04-22 15:29:52 +0000

    Apr 22, 2025, 5:29 PM

---
+++ camel/camel.py
@@ -0,0 +1,13 @@
+### Convert camelCase into snake_case ###
+
+def main():
+    camel = input("camelCase: ")
+    
+    for i in camel:
+        if i.isupper():
+            camel.split(i)
+            
+            
+    
+
+main()
