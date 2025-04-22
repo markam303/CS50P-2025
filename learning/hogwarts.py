f0640a79b7fbc03c03aa3049c4f991ca3b794805
markam303 <marcin93kaminski@gmail.com>
2025-04-22 14:51:43 +0000

    Apr 22, 2025, 4:51 PM

--- learning/hogwarts.py
+++ learning/hogwarts.py
@@ -1,9 +1,10 @@
-students = {"Hermione": "Gryffindor",
-            "Harry": "Gryffindor",
-            "Ron": "Gryffindor",
-            "Draco": "Slitherin",
-}
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slitherin", "patronus": None},    
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], students["house"], students["patronus"], sep=", ")
     
