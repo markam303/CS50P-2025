4422f9072a0c48952b0b6cd9f3402ad48107b4c9
markam303 <marcin93kaminski@gmail.com>
2025-04-22 18:31:33 +0000

    Apr 22, 2025, 8:31 PM

--- coke/coke.py
+++ coke/coke.py
@@ -1,14 +1,13 @@
 ### Coke machine that checks for amount due from the user. Cost of 1 cola is 50 cents ###
 coins = [25, 10, 5]
-amount_due = 50
 
 def main():
-    # amount_due = 50
+    amount_due = 50
     
     while amount_due >= 0:
         print(f"Amount Due: {amount_due}")
         insert = get_coin()
-        calc_amount_due(insert)
+        amount_due = calc_amount_due(insert)
         
 
 def get_coin():
