c9d53bd6355f9881503b0903d177a7c35cbe0407
markam303 <marcin93kaminski@gmail.com>
2025-04-22 19:55:18 +0000

    Apr 22, 2025, 9:55 PM

---
+++ twttr/twttr.py
@@ -0,0 +1,14 @@
+### Remove vowels from strings ###
+
+def main():
+    text = input("Input: ")
+    removed_vowels = remove(text)
+    print(removed_vowels)
+
+def remove(text):
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    for i in vowels:
+        text.replace(i, "")
+
+
+main()
