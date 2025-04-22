c493551c3f5aea938385ae57432145f74fd731fb
markam303 <marcin93kaminski@gmail.com>
2025-04-22 18:37:05 +0000

    Apr 22, 2025, 8:37 PM

--- coke/coke.py
+++ coke/coke.py
@@ -2,12 +2,13 @@
 coins = [25, 10, 5]
 
 def main():
-    
+    amount_due = 50
     while amount_due >= 0:
-        amount_due = 50
+        # amount_due = 50
         print(f"Amount Due: {amount_due}")
         insert = get_coin()
-        amount_due = calc_amount_due(insert)
+        amount_due = calc_amount_due(amount_due, insert)
+
         
 
 def get_coin():
