7e008c5624c4c62b5538ad9a5e72a7a9056da623
markam303 <marcin93kaminski@gmail.com>
2025-04-23 12:29:21 +0000

    Apr 23, 2025, 2:29 PM

--- plates/plates.py
+++ plates/plates.py
@@ -17,7 +17,7 @@ def is_valid(plate):
 
 # Check if first 2 chars are letters
 def check_2_first_letters(plate):
-    twofirst = plate.slice[0:2]
+    twofirst = plate[0:2]
     if twofirst.isalpha():
         return 0
     else:
