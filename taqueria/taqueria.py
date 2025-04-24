ed6c66e925d941285353c1afb6380105da353fb4
markam303 <marcin93kaminski@gmail.com>
2025-04-24 20:19:18 +0000

    Apr 24, 2025, 10:19 PM

---
+++ taqueria/taqueria.py
@@ -0,0 +1,27 @@
+### Place an order at Felipe's Taqueria ###
+
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+    try:
+        item = input("Item: ").title()
+        total = 0
+        for item in menu:
+            price = menu.get(item)
+            total += price
+        print(f"${total}")
+    except EOFError:
+        print()
+
+
+main()
