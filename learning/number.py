6ea1671c7705a9de4ff1011690510d48ceb084aa
markam303 <marcin93kaminski@gmail.com>
2025-04-24 15:24:23 +0000

    Apr 24, 2025, 5:24 PM

--- learning/number.py
+++ learning/number.py
@@ -1,7 +1,7 @@
-
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not a number")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not a number")
+    else:
+        print(f"x is {x}")
