3a88886cbdb3164e2b232ad660dc96908ad7775a
markam303 <marcin93kaminski@gmail.com>
2025-04-26 20:36:27 +0000

    Apr 26, 2025, 10:36 PM

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -9,7 +9,7 @@ try:
     while True:
         item = input("").upper()
         if item not in grocery_list:
-            grocery_list.update({item: 1})
+            grocery_list.append({item: 1})
         elif item in grocery_list:
             grocery_list[item] += 1
            
