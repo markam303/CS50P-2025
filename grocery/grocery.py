0d4b91b9d958320eaf2c0626323568235c074c5b
markam303 <marcin93kaminski@gmail.com>
2025-04-26 18:32:54 +0000

    Apr 26, 2025, 8:32 PM

---
+++ grocery/grocery.py
@@ -0,0 +1,8 @@
+### Grocery List ###
+# This program allows a user to create a grocery list, add items to it, and view the list.
+
+grocery_list = []  # Initialize an empty grocery list
+while True:
+    item = input("")
+    grocery_list.append(item)
+    grocery_list = sorted(grocery_list)
