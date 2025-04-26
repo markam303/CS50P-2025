9c734469608df6f6c4be04af6bea594d425947ae
markam303 <marcin93kaminski@gmail.com>
2025-04-26 18:36:44 +0000

    Apr 26, 2025, 8:36 PM

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,7 +2,12 @@
 # This program allows a user to create a grocery list, add items to it, and view the list.
 
 grocery_list = []  # Initialize an empty grocery list
-while True:
-    item = input("").upper()
-    grocery_list.append(item)
-    grocery_list = sorted(grocery_list)
+
+try:
+    while True:
+        item = input("").upper()
+        grocery_list.append(item)
+        grocery_list = sorted(grocery_list)
+except EOFError:
+    for item in grocery_list:
+        print(item)
