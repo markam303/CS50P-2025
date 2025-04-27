8bac2b7308543007c463c8fa350ff28820ca4ce3
markam303 <marcin93kaminski@gmail.com>
2025-04-27 13:40:15 +0000

    Apr 27, 2025, 3:40 PM

---
+++ outdated/outdated.py
@@ -0,0 +1,20 @@
+### Date conversion program ###
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+date = input("Date: ")
+month, day, year = date.split("/")
+print(f"{year}-{month:02}-{day:02}")
