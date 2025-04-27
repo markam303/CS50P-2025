5ffbab6d4050d0f79469377f56dcbfb6e36f8ba0
markam303 <marcin93kaminski@gmail.com>
2025-04-27 16:52:47 +0000

    Apr 27, 2025, 6:52 PM

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -33,6 +33,7 @@ def main():
 def check_format(date):
     try:
         month, day, year = date.split(" ")
+        day = int(day)
         day = day.strip(",")
         month = (months.index(month) + 1)
         return day, month, year
