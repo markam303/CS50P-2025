acdcd5cbcddf1287f206a827317f36dde3ed3f97
markam303 <marcin93kaminski@gmail.com>
2025-04-30 12:23:05 +0000

    Apr 30, 2025, 2:23 PM

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,12 @@
+### Bitcoin Price Index ###
+import sys
+import requests
+
+
+def main():
+    if (sys.argv) != 2:
+        sys.exit("Usage: bitcoin.py <float>")
+        
+        
+if __name__ == "__main__":
+    main()
