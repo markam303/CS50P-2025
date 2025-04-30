7c4c5085b75b3d720384d099c90944bdc580d0d3
markam303 <marcin93kaminski@gmail.com>
2025-04-30 12:25:54 +0000

    Apr 30, 2025, 2:25 PM

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,7 +6,8 @@ import requests
 def main():
     if (sys.argv) != 2:
         sys.exit("Usage: bitcoin.py <float>")
-        
+    
+    API_key = "4862c850dd068878bacc7bd19ca8c5395806430360b8fbb48597a48bd2cf52ad"
         
 if __name__ == "__main__":
     main()
