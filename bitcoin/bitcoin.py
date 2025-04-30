526584bd0936302e6e304ecaef8954336a54026e
markam303 <marcin93kaminski@gmail.com>
2025-04-30 15:27:26 +0000

    Apr 30, 2025, 5:27 PM

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -10,7 +10,8 @@ def main():
     
     API_key = "4862c850dd068878bacc7bd19ca8c5395806430360b8fbb48597a48bd2cf52ad"
     response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=" + API_key)
-    print(response)
+    print(json.dumps(response.json(), indent=2))
+    
     # print(f"${amount:,.4f}")
         
 if __name__ == "__main__":
