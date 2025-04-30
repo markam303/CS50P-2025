0b4f1a5b9fcc5cc1039162a1dfa3eb1deea0dc44
markam303 <marcin93kaminski@gmail.com>
2025-04-30 16:47:41 +0000

    Apr 30, 2025, 6:47 PM

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -9,7 +9,8 @@ def main():
         sys.exit("Usage: bitcoin.py <float>")
     
     API_key = "4862c850dd068878bacc7bd19ca8c5395806430360b8fbb48597a48bd2cf52ad"
-    response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=" + API_key)
+    url = "https://rest.coincap.io/v3/assets/bitcoin?apiKey="
+    response = requests.get(url + API_key)
     
     o = response.json()
     o = json.loads(o)
