d4ed48a959f8a6315a9c7412fd657a411fc7f72c
markam303 <marcin93kaminski@gmail.com>
2025-05-05 10:00:16 +0000

    May 5, 2025, 12:00 PM

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -2,16 +2,36 @@ from twttr import shorten
 import pytest
     
 
-def test_text():
+def test_vowels():
     assert shorten("twitter") == "twttr"
+    
+    
+def test_consonants_only():    
     assert shorten("CS50") == "CS50"
-    assert shorten("What's your name?") == "Wht's yr nm?"
+    
+    
+def test_omitting_punctuation():
+    assert shorten("what's your name?") == "wht's yr nm?"
 
 
-def test_number():
+def test_omitting_numbers():
+    ...
+
+
+def test_all_uppercase():
+    ...
+
+
+def test_lowercase_vowels():
+    ...
+
+
+def test_capitalized_vowel():
+    ...
+
+
+def test_integers():
     with pytest.raises(AttributeError):
         shorten(1)
         shorten(0)
         shorten(-1)
-
-
