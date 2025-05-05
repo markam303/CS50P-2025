1471306175fa4b39573aa4a31ddb72580491b27e
markam303 <marcin93kaminski@gmail.com>
2025-05-05 10:56:31 +0000

    May 5, 2025, 12:56 PM

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -4,12 +4,14 @@ from bank import value
 
 def test_hello():
     assert value("hello") == 0
+    assert value("Hello") == 0
     
 
 def test_different():
     assert value("what's up") == 100
+    assert value("Great") == 100
 
 
 def test_first():
     assert value("hey") == 20
-    assert value("hiho") == 20
+    assert value("Hiho") == 20
