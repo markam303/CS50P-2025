79186ae90da6e013cdff567fcd950b34c8a2ff8f
markam303 <marcin93kaminski@gmail.com>
2025-05-05 11:24:00 +0000

    May 5, 2025, 1:24 PM

---
+++ test_plates/test_plates.py
@@ -0,0 +1,21 @@
+import pytest
+from plates import is_valid
+
+
+def test_first_2_letters():
+    assert is_valid("AA") == True
+    assert is_valid("AAdid") == True
+    assert is_valid("0Z0N") == False
+    
+
+def test_length():
+    assert is_valid("AB") == True
+    assert is_valid("ABRACA") == True
+    assert is_valid("ABRACAD") == False
+    
+
+def test_numbers():
+    assert is_valid("AA007") == False
+    assert is_valid("AA107") == True
+    assert is_valid("AA1000") == False
+
