546466242ebe774dd2c785c866cb2db91f73251f
markam303 <marcin93kaminski@gmail.com>
2025-05-06 14:49:52 +0000

    May 6, 2025, 4:49 PM

---
+++ lines/lines.py
@@ -0,0 +1,13 @@
+# Count number of lines in *.py file, given in CLI
+
+import sys
+
+
+def main():
+    # Check for number and type of arguments
+    if len(sys.argv) != 2:
+        sys.exit()
+        
+
+if __name__ == "__main__":
+    main()
