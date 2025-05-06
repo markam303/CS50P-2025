7de3e2f467714cae7db93bc0a7b96878e038167b
markam303 <marcin93kaminski@gmail.com>
2025-05-06 09:34:41 +0000

    May 6, 2025, 11:34 AM

--- names.py
+++ names.py
@@ -1,6 +1,7 @@
 names = []
 
 for _ in range(3):
-    name = input("What's your name? ")
-    names.append(name)
-print(f"hello, {name}")
+    names.append(input("What's your name? "))
+
+for name in sorted(names):
+    print(f"hello, {name}")
