c56046e5e48c22c795a243236b7c6c325bb37a89
markam303 <marcin93kaminski@gmail.com>
2025-05-06 10:37:59 +0000

    May 6, 2025, 12:37 PM

--- students.py
+++ students.py
@@ -7,9 +7,5 @@ with open("students.csv") as file:
         students.append(student)
         
 
-def get_name(student):
-    return student["name"]
-
-
-for student in sorted(students, key=get_name):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
