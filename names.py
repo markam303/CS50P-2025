2baa044effc38bbe2dc711798fc1a0696c8d6354
markam303 <marcin93kaminski@gmail.com>
2025-05-06 10:06:07 +0000

    May 6, 2025, 12:06 PM

--- names.py
+++ names.py
@@ -1,8 +1,3 @@
-names = []
-
-with open("names.txt") as file:
-    for line in file:
-        names.append(line.rstrip())
-        
-for name in sorted(names):
-    print(f"hello, {name}")        
+with open("names.txt", "r") as file:
+    for line in sorted(file):
+        print("hello,", line.rstrip())
