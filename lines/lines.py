f5874e7e9bd0e4bd52a131e5653907c284b68545
markam303 <marcin93kaminski@gmail.com>
2025-05-06 15:02:55 +0000

    May 6, 2025, 5:02 PM

--- lines/lines.py
+++ lines/lines.py
@@ -1,13 +1,27 @@
-# Count number of lines in *.py file, given in CLI
+# Count number of lines in *.py file, provided as CLI argument
 
 import sys
 
 
 def main():
     # Check for number and type of arguments
-    if len(sys.argv) != 2:
-        sys.exit()
-        
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    else:
+        pass
+    
+
+def check_extension(str):
+    try:
+        a, b = str.split(".")
+    except ValueError:
+        return False
+    if b == "py":
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
