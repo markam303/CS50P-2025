6e8251be7a1fb241038b8f62f459c6c142e5bf06
markam303 <marcin93kaminski@gmail.com>
2025-05-07 14:10:59 +0000

    May 7, 2025, 4:10 PM

--- lines/lines.py
+++ lines/lines.py
@@ -11,24 +11,21 @@ def main():
         sys.exit("Too many command-line arguments")
     elif not sys.argv[1].endswith(".py"):
         sys.exit("Not a Python file")
-    else:    
-        try:
-            file = open(sys.argv[1])       
-        except FileNotFoundError:
-            sys.exit("File does not exist")
+         
+    try:
+        with open(sys.argv[1]) as file:
+            count = 0
+            for line in file:
+                stripped = line.strip()
+                if not stripped:
+                    continue
+                if stripped.startswith("#"):
+                    continue
+                count += 1
+        print(count)
+    except FileNotFoundError:
+        sys.exit("File does not exist")
     
-    # Count lines, with whitespaces, comments excluded
-    count = 0
-    for line in file:
-        if line[0].lstrip().startswith("#") or line[0].removeprefix(" ").startswith("\n") or line[0].isspace():
-            count += 0
-        else:
-            count += 1
-    
-    print(count)
-    
-    file.close()
         
-
 if __name__ == "__main__":
     main()
