e008e5acef80eb730d7728805d5bf6279f8dffec
markam303 <marcin93kaminski@gmail.com>
2025-05-07 15:57:06 +0000

    May 7, 2025, 5:57 PM

---
+++ scourgify/scourgify.py
@@ -0,0 +1,30 @@
+# Scourgify - sorting csv file to easily mail merge
+"""Provide one csv file to sort as first argument and new filename as second argument"""
+
+import sys
+
+import csv
+
+def main():
+    # Check for number of CLA and its type
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    elif not sys.argv[1].endswith(".csv"):
+        sys.exit("Wrong file format")
+    
+    list = []
+    with open(sys.argv[1]) as file_input:
+        reader = csv.DictReader(file_input)
+        for row in reader:
+            list.append(row)
+    
+    with open(sys.argv[2], "w") as file_output:
+        writer = csv.DictWriter
+        
+    
+    
+    
+if __name__ == "__main__":
+    main()
