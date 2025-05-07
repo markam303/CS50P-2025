f5336b6fdbd0d5b349dfc16ecdc0f66194aa8895
markam303 <marcin93kaminski@gmail.com>
2025-05-07 14:43:59 +0000

    May 7, 2025, 4:43 PM

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,14 +7,19 @@ from tabulate import tabulate
 
 def main():
     # Check for number and type of CLA
-    if not len(sys.argv[1]) == 2:
-        sys.exit("Provide only one *.csv file")
+    if len(sys.argv[1]) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv[1]) > 2:
+        sys.exit("Too many command-line arguments")
     elif not sys.argv[1].endswith(".csv"):
-        sys.excit("Wrong file format")
+        sys.exit("Not a CSV file")
     
     try:
-        ...
+        with open(sys.argv[1]) as file:
+            print(tabulate(file))
     except FileNotFoundError:
         sys.exit("File not")
+        
+        
 if __name__ == "__main__":
     main()
