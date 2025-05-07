884bdc5c6c53f92717a63d618a3b252b6991275f
markam303 <marcin93kaminski@gmail.com>
2025-05-07 14:40:41 +0000

    May 7, 2025, 4:40 PM

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,1 +1,20 @@
-# Pizza Py
+# Pizza Py - provide *.csv file as CLA
+
+import sys
+
+from tabulate import tabulate
+
+
+def main():
+    # Check for number and type of CLA
+    if not len(sys.argv[1]) == 2:
+        sys.exit("Provide only one *.csv file")
+    elif not sys.argv[1].endswith(".csv"):
+        sys.excit("Wrong file format")
+    
+    try:
+        ...
+    except FileNotFoundError:
+        sys.exit("File not")
+if __name__ == "__main__":
+    main()
