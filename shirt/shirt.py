f7291d45810247345b592b45c398a0d128d6ce2b
markam303 <marcin93kaminski@gmail.com>
2025-05-08 10:32:56 +0000

    May 8, 2025, 12:32 PM

---
+++ shirt/shirt.py
@@ -0,0 +1,20 @@
+"""CS50 P-Shirt. 
+Provide input img as first CLA and name of output img as second CLA.
+Possible extensions: jpg, jpeg, png.
+"""
+
+import sys
+
+
+def main():
+    # Check for CLA
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    
+    
+
+
+if __name__ == "__main__":
+    main()
