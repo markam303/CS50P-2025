cfea9eb614c9483869b6446be8bed1ef892072d6
markam303 <marcin93kaminski@gmail.com>
2025-05-09 12:24:18 +0000

    May 9, 2025, 2:24 PM

--- validate.py
+++ validate.py
@@ -3,7 +3,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@(\w+\.)?\w+\.(com|edu|net|pl|org)$", email):
+if re.search(r"^[a-z0-9_\.]+@(\w+\.)?\w+\.(com|edu|net|pl|org)$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
