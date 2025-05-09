8e91fd1ae6ea7c9d86adb9f34f9c6d3402a237a6
markam303 <marcin93kaminski@gmail.com>
2025-05-09 13:07:10 +0000

    May 9, 2025, 3:07 PM

--- twitter.py
+++ twitter.py
@@ -6,6 +6,6 @@ url = input("URL: ").strip()
 # username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 # print(f"Username: {username}")
 
-matches = re.search(r"^https://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+matches = re.search(r"^https://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
 if matches:
-    print(f"Username:", matches.group(2))
+    print(f"Username:", matches.group(1))
