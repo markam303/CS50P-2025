9aa366db472554dfb9323dcfb2ce349d0c527ec2
markam303 <marcin93kaminski@gmail.com>
2025-05-09 15:24:24 +0000

    May 9, 2025, 5:24 PM

---
+++ numb3rs/numb3rs.py
@@ -0,0 +1,19 @@
+""""Validate IP adress (user's input)"""
+
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    
+
+
+    ...
+
+
+if __name__ == "__main__":
+    main()
