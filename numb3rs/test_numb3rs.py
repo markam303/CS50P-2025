e7bf0ce699fad8a8a2e9a562120f3c34ca415975
markam303 <marcin93kaminski@gmail.com>
2025-05-09 16:23:28 +0000

    May 9, 2025, 6:23 PM

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -7,8 +7,11 @@ def test_string():
     assert validate("cat") == False
     assert validate("cat.cat.cat.cat") == False
      
-def test_range():
+def test_boundaries():
     assert validate("0.0.0.0") == True
     assert validate("255.255.255.255") == True
+    assert validate("12.12.11.12") == True
+
+def test_wrong():
     assert validate("255") == False
     assert validate("256.4.5.2") == False
