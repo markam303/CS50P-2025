5260b4c7510138667b73ba3639ebfbee7b44c214
markam303 <marcin93kaminski@gmail.com>
2025-05-09 12:57:15 +0000

    May 9, 2025, 2:57 PM

--- twitter.py
+++ twitter.py
@@ -3,5 +3,5 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"https://twitter.com/", "", url)
+username = re.sub(r"^https://twitter\.com/", "", url)
 print(f"Username: {username}")
