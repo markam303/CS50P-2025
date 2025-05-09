2edd7b4e42f9d53b6cfbbb095cc5bb9a680fa308
markam303 <marcin93kaminski@gmail.com>
2025-05-09 12:41:47 +0000

    May 9, 2025, 2:41 PM

--- format.py
+++ format.py
@@ -1,7 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), ?(.+)$", name)
+matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
     # last, first = matches.groups()
     # last = matches.group(1)
