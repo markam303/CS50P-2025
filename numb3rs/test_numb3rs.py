56b0e153e82b067e45e61bba982bc4cdb8ccb15a
markam303 <marcin93kaminski@gmail.com>
2025-05-09 16:08:19 +0000

    May 9, 2025, 6:08 PM

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,3 +2,13 @@ import pytest
 
 from numb3rs import validate
 
+
+def test_string():
+    assert validate("cat") == False
+    assert validate("cat.cat.cat.cat") == False
+     
+def test_range():
+    assert validate("0.0.0.0") == True
+    assert validate("255.255.255.255") == True
+    assert validate("255") == False
+    assert validate("256.4.5.2") == False
