f00a5582d911609888e9b048109dcde32b3695c6
markam303 <marcin93kaminski@gmail.com>
2025-05-12 15:54:33 +0000

    May 12, 2025, 5:54 PM

--- working/working.py
+++ working/working.py
@@ -11,11 +11,11 @@ def convert(s):
         ^\s*                # Optional leading ws
         (1[0-2]|0?[1-9])    # first hour group with possible leading 0
         (?::([0-5][0-9]))?  # non-capturing group of minutes group (captured minutes itself)
-        \s*(AM|PM)          # AM PM with leading ws
+        \s+(AM|PM)          # AM PM with leading ws
         \s*to\s*            # to with leading and trailing ws
         (1[0-2]|0?[1-9])    # second hour group with possible leading 0
         (?::([0-5][0-9]))?  # non-capturing group of minutes group (captured minutes itself) as above
-        \s*(AM|PM)          # AM PM with leading ws
+        \s+(AM|PM)          # AM PM with leading ws
         \s*$                # trailing ws
         """
     match = re.match(pattern, s, re.X)
