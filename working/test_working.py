e765f0370fe82d28f26d7fbb0c150a8e2135aa5a
markam303 <marcin93kaminski@gmail.com>
2025-05-12 15:32:09 +0000

    May 12, 2025, 5:32 PM

--- working/test_working.py
+++ working/test_working.py
@@ -7,4 +7,20 @@ def test_fullinput():
 
 
 def test_nominutes():
-    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    
+
+def test_semiinput():
+    assert convert("9:00 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    
+def test_invertedinput():
+    assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"    
+
+def test_wronginput():
+    with pytest.raises(ValueError):
+        convert("cat")
+        convert("9:60 AM to 5:60 PM")
+        convert("9 AM - 5 PM")
+        convert("09:00 AM - 17:00 PM")
+        
