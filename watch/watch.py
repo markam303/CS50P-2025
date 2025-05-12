daa5e20957e198bd2137b21a5afb91e1d1f23b81
markam303 <marcin93kaminski@gmail.com>
2025-05-12 10:13:36 +0000

    May 12, 2025, 12:13 PM

--- watch/watch.py
+++ watch/watch.py
@@ -1,2 +1,19 @@
 """Input embedded youtube iframe and output shareable youtube link"""
 
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
