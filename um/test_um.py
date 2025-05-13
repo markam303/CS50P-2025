d6e3bdbe9b3bd77cecdd80613a09d611ac0c6c8a
markam303 <marcin93kaminski@gmail.com>
2025-05-13 14:44:54 +0000

    May 13, 2025, 4:44 PM

---
+++ um/test_um.py
@@ -0,0 +1,18 @@
+import pytest
+from um import count
+
+
+def test_single_um():
+    assert count("um") == 1
+
+
+def test_punctuation_um():
+    assert count("um?") == 1
+    
+
+def test_uppercase_um():
+    assert count("Um, thanks for the album.") == 1
+    
+
+def test_dotdotdot_um():
+    assert count("Um, thanks, um...")
