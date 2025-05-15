1e0341f95694d8bb7edf10508fdb045a632ddb1e
markam303 <marcin93kaminski@gmail.com>
2025-05-15 12:50:19 +0000

    May 15, 2025, 2:50 PM

--- student.py
+++ student.py
@@ -4,10 +4,9 @@ def main():
 
 
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
 
 # Return list
 """ def get_student():
