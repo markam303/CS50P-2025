668d27d8a968da3e4f4ff398a63f0867f97d5426
markam303 <marcin93kaminski@gmail.com>
2025-05-15 13:30:59 +0000

    May 15, 2025, 3:30 PM

--- student.py
+++ student.py
@@ -1,7 +1,9 @@
 class Student:
     def __init__(self, name, house):
         if not name:
-            
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
