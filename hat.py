bd08be30e151dac15ddcf01c6731fb9cc26d5838
markam303 <marcin93kaminski@gmail.com>
2025-05-15 16:05:46 +0000

    May 15, 2025, 6:05 PM

--- hat.py
+++ hat.py
@@ -1,11 +1,12 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    
+    @classmethod    
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
         
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
-        
-hat = Hat()
-hat.sort("Harry")
+
+Hat.sort("Harry")
