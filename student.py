303575ac8f1cbec37b6b84cdd7e71060913e6d23
markam303 <marcin93kaminski@gmail.com>
2025-05-15 13:41:08 +0000

    May 15, 2025, 3:41 PM

--- student.py
+++ student.py
@@ -6,10 +6,14 @@ class Student:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        
+    def __str__(self):
+        return "a student"
 
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
+    print(student)
 
  
 # Return class
