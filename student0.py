9289175ecc23686a62138c631ca65e785adf0188
markam303 <marcin93kaminski@gmail.com>
2025-05-15 16:20:49 +0000

    May 15, 2025, 6:20 PM

---
+++ student0.py
@@ -0,0 +1,106 @@
+class Student:
+    def __init__(self, name, house):  # patronus
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        # self.patronus = patronus
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+        
+    # Getter
+    @property
+    def house(self):
+        return self._house
+    
+    # Setter
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+# Function inside class
+"""     def charm(self):
+        match self.patronus:
+            case "Stag":
+                return ":horse:"
+            case "Otter":
+                return ":otter"
+            case "Jack Russell terrier":
+                return ":dog:"
+            case _:
+                return "emoji_wand_default"       """  
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+    print(student)
+    
+    # print("Expecto Patronum!")
+    # print(student.charm())
+    
+ 
+# Return class
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    # patronus = input("Patronus: ")
+    return Student(name, house) # patronus
+
+
+
+# Return class
+""" def get_student():
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student    """ 
+
+
+# Return dictionary
+""" def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house} """
+
+
+# Return list
+""" def get_student():
+    name = input("Name ")
+    house = input("House ")
+    return [name, house] """
+
+
+# Return tuple
+""" def get_student():
+    name = input("Name ")
+    house = input("House ")
+    return name, house """
+
+
+# Return values separately
+""" def get_name():
+    return input("Name: ")
+
+def get_house():
+    return input("House ") """
+
+
+if __name__ == "__main__":
+    main()
