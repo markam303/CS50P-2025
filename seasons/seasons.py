61208da2bc7699d4ac3689342274b3cf5b19feb1
markam303 <marcin93kaminski@gmail.com>
2025-05-16 15:30:39 +0000

    May 16, 2025, 5:30 PM

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,17 +1,25 @@
 from datetime import date, timedelta
 import sys
-import re
+
 
 
 def main():
-    birthday = input("Date of Birthday: ")
+    birthday = get_date("Date of Birthday: ")
     birthday = date.fromisoformat(birthday)
     today = date.today()
     delta = today - birthday
-    delta2 = delta.minute
+
     print(delta)
-    print(delta2)
- 
+
+
+def get_date(s):
+    d = str(input(s))
+    try:
+        d = date.fromisoformat(d)
+    except ValueError:
+        sys.exit("Invalid date")
+    return d
+    
 
 ...
 
