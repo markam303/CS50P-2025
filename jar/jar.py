ff344579777132e2e30c0bc824a72f9b4a38502f
markam303 <marcin93kaminski@gmail.com>
2025-05-16 20:03:53 +0000

    May 16, 2025, 10:03 PM

--- jar/jar.py
+++ jar/jar.py
@@ -2,21 +2,25 @@ class Jar:
     def __init__(self, capacity=12):
         if capacity < 0 or not capacity:
             raise ValueError("Wrong capacity")
+        if capacity > 12:
+            raise ValueError("Jar is full")
         self.capacity = capacity
 
     def __str__(self):
-        return f"🍪" * self.capacity
+        return f"🍪" * self.size
 
     def deposit(self, n):
-        self.capacity += n
-        return self.capacity 
+        self.size += n
+        return self.size 
     
     def withdraw(self, n):
-        ...
+        self.size -= n
+        return self.size
 
     @property
     def capacity(self):
-        ...
+        self._capacity = capacity
+        return self._capacity
 
     @property
     def size(self):
@@ -24,7 +28,8 @@ class Jar:
 
 
 def main():
-    ...
+    print(Jar.capacity())
+    
 
 
 if __name__ == "__main__":
