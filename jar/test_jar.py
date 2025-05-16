da20fc4035340910cf3be9afdb988650bad5177a
markam303 <marcin93kaminski@gmail.com>
2025-05-16 22:02:40 +0000

    May 17, 2025, 12:02 AM

---
+++ jar/test_jar.py
@@ -0,0 +1,29 @@
+import pytest
+
+from jar import Jar
+
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    with pytest.raises(ValueError):
+        jar = Jar()
+        jar.deposit(20)
+
+
+def test_withdraw():
+    with pytest.raises(ValueError):
+        jar = Jar()
+        jar.withdraw(20)
