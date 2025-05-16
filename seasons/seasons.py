ed8e6d81ba46dc859ea49f3f7d69b719098fda5a
markam303 <marcin93kaminski@gmail.com>
2025-05-16 16:42:13 +0000

    May 16, 2025, 6:42 PM

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -3,24 +3,23 @@ import sys
 import inflect
 
 
-
 def main():
     try:
         birthday = get_date("Date of Birthday: ")
     except ValueError:
         sys.exit("Invalid date")
-        
+
     today = date.today()
     delta = today - birthday
-    
+
     delta = delta.total_seconds()
     minutes = convert_to_min(delta)
-    print(minutes)
+
     speller = minute_speller(minutes)
     print(speller)
-    
-    
-def minute_speller(minutes):    
+
+
+def minute_speller(minutes):
     p = inflect.engine()
     words = p.number_to_words(minutes, andword="").capitalize()
     plural_minutes = p.plural_noun("minute", count=minutes)
@@ -33,7 +32,7 @@ def get_date(s):
 
 def convert_to_min(seconds):
     return int(seconds / 60)
-    
+
 
 if __name__ == "__main__":
     main()
