3690221c4360b886a9bdb0bfa7ae0bd7af053512
markam303 <marcin93kaminski@gmail.com>
2025-05-16 14:29:29 +0000

    May 16, 2025, 4:29 PM

---
+++ seasons/seasons.py
@@ -0,0 +1,16 @@
+from datetime import date
+import sys
+import re
+
+
+def main():
+    birthday = input("Date of Birthday: ")
+    birthday = date.fromisoformat(birthday)
+
+        
+
+...
+
+
+if __name__ == "__main__":
+    main()
