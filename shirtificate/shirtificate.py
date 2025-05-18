4e79a3ccce4979bcee1290c1fba2aed869a0aaa1
markam303 <marcin93kaminski@gmail.com>
2025-05-18 15:56:54 +0000

    May 18, 2025, 5:56 PM

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,15 +7,17 @@ class PDF(fpdf.FPDF):
         # Setting font:
         self.set_font("helvetica", style="B", size=40)
         # Printing title:
-        self.cell(0, 10, "CS50 Shirtificate", align="C")
+        self.cell(0, 50, "CS50 Shirtificate", align="C")
         # Performing a line break:
         self.ln(20)
 
 
 def main():
+    name = get_name()
     pdf = PDF()
     pdf.add_page()
-
+    pdf.set_auto_page_break(False)
+    
     pdf.image("shirtificate.png",
               h=pdf.eph,
               w=pdf.epw,
@@ -23,8 +25,13 @@ def main():
               keep_aspect_ratio=True,
               )
     
+    pdf.set_y(pdf.eph / 2)
+    pdf.set_font("Times", style="B", size=28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(0, 10, f"{name} took CS50", align="C")
+    
     pdf.output("my-shirt.pdf")
-    pdf.set_auto_page_break(False)
+    
     
 
 def get_name():
