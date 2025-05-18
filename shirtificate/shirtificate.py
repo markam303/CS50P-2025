3ad478d8d1ca602e4108a2a26edfa992a8a2883e
markam303 <marcin93kaminski@gmail.com>
2025-05-18 14:12:44 +0000

    May 18, 2025, 4:12 PM

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -9,7 +9,7 @@ class PDF(fpdf.FPDF):
         # Moving cursor to the right:
         self.cell(80)
         # Printing title:
-        self.cell(30, 10, "Title", border=1, align="C")
+        self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
         # Performing a line break:
         self.ln(20)
 
