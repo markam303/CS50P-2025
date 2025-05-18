3211f4e88c35d933f317093bacc79a8efa5bd5cc
markam303 <marcin93kaminski@gmail.com>
2025-05-18 13:58:29 +0000

    May 18, 2025, 3:58 PM

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ from PIL import Image
 def main():
     pdf = fpdf.FPDF()
     pdf.add_page()
-    background_image = "shirtificate.png"
+    background_image = Image.open("shirtificate.png")
     # pdf.allow_images_transparency = False
     pdf.set_page_background(background_image)
     pdf.set_auto_page_break = False
