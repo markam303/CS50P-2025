f873cf8992b9a7a0453f55953e77bbce2836eaa5
markam303 <marcin93kaminski@gmail.com>
2025-05-18 14:02:52 +0000

    May 18, 2025, 4:02 PM

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,7 +7,11 @@ def main():
     pdf.add_page()
     background_image = Image.open("shirtificate.png")
     # pdf.allow_images_transparency = False
-    pdf.image(background_image, alt_text="CS50 T-Shirt")
+    pdf.image(background_image,
+              h=pdf.eph,
+              w=pdf.epw,
+              alt_text="CS50 T-Shirt",
+              )
     pdf.set_auto_page_break = False
     pdf.output("my-shirt.pdf")
     
