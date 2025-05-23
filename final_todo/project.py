10a9a7be846c7326312fdc74c18d41d834cb469f
markam303 <marcin93kaminski@gmail.com>
2025-05-23 14:29:36 +0000

    May 23, 2025, 4:29 PM

--- final_todo/project.py
+++ final_todo/project.py
@@ -83,7 +83,7 @@ def add_task(description: str, priority) -> bool:
         )
 
         tasks.append(new_task)
-        print(f"Task added: {new_task.task_id} {new_task.description} (Priority: {new_task.priority})")
+        print(f"Task added: {new_task.id} {new_task.description} (Priority: {new_task.priority})")
         save_tasks()
         return True
 
