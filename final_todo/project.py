ac878a616da544d4d3de7cb629f1946c54b24528
markam303 <marcin93kaminski@gmail.com>
2025-05-24 13:00:04 +0000

    May 24, 2025, 3:00 PM

--- final_todo/project.py
+++ final_todo/project.py
@@ -15,7 +15,7 @@ class Task:
         if not description or not description.strip():
             raise ValueError("Error: Missing task description")
         # Allow letters, numbers, spaces, punctuation
-        if not self.validate_description:
+        if self.validate_description:
             raise ValueError("Error: Allowed characters are letters, numbers, spaces and punctuation")
         
 
